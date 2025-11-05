1fd3238ff83b43ac89a5a8dbd8687acfc225ead9
Andriy-x64 <andriydovhopolyuk@gmail.com>
2025-11-06 01:55:21 +0200

    feat: ask age and print adult/underage in console

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -7,8 +7,15 @@ namespace App
         {
             Console.Write("Введіть ваше ім'я: ");
             string userName = Console.ReadLine();
-            Console.WriteLine($"\nВітаю {userName}!");
 
+             Console.Write("Введіть ваш вік: ");
+            string userAgeInput = Console.ReadLine();
+            int userAge;
+            if (int.TryParse(userAgeInput, out userAge))
+            {
+                Console.WriteLine($"\nВітаю {userName}!");
+                Console.WriteLine($"Ваш вік: {userAge} років.");
+            }
             Console.ReadKey();
         }
     }
